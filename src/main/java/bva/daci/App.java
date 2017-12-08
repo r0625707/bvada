e2e2a0a5ba98d4ebccaace08ea485930294de534
Bram <bram.vanasschodt@student.ucll.be>
2017-12-08 12:25:33 +0100

    hello world project

---
+++ src/main/java/bva/daci/App.java
@@ -0,0 +1,13 @@
+package bva.daci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
